3a889e3813519652c4c6581a5fc6b9c91ed3abcb
qwertyfonz <qwertyfonz@DESKTOP-7RAHV6L.localdomain>
2020-06-29 00:09:09 -0400

    Created page.html for entries, which is auto-populated by page function in views.py, and created links from index to pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
 
+from markdown2 import Markdown 
+
+markdowner = Markdown()
+
+def siteexists(entry):
+    if util.get_entry(entry) == None:
+        return False
+    return True
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+# When this is called, we want to render the template page.html and pass in the title of our page to the get_entry function
+# We want to use the markdown function to convert the page into HTML before displaying it to the user using markdowner.convert(content)
+def page(request, entry):
+    if siteexists(entry) == False:
+        return HttpResponse("No page found.")
+    else:
+        return render(request, "encyclopedia/page.html", {
+            "entry": markdowner.convert(util.get_entry(entry))
+        })
+
 
